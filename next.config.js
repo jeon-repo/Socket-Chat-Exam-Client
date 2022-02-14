ad8fa350dd4f14e796986258df81ef946ba76145
jeon-repo <jano.jeon@gmail.com>
2022-02-14 11:25:00 +0900

    🔧 redirect, rewrite 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // comment test
+  async redirects() {
+    return [
+      {
+        source: '/api/test-redirect/:path*',
+        destination: 'https://www.google.com/:path*',
+        permanent: false
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/test-rewrite',
+        destination: `http://www.naver.com`
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 function Home() {
   return (
     <div>
+      <h1>&lt;내부 페이지&gt;</h1>
       <h1>
         <Link href="/user">
           <a>유저 화면으로 이동</a>
@@ -11,6 +12,17 @@ function Home() {
       <h1>
         <Link href="/mobile">
           <a>모바일 화면으로 이동</a>
+        </Link>
+      </h1>
+      <h1>&lt;외부 페이지&gt;</h1>
+      <h1>
+        <Link href="/api/test-redirect">
+          <a>redirect 테스트 화면으로 이동</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/api/test-rewrite">
+          <a>rewrite 테스트 화면으로 이동</a>
         </Link>
       </h1>
     </div>
